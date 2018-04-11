47cc1aef5b5702b6bb0d74d7d2b87ac8393565ae
APelevin <pelevin_a84@mail.ru>
2018-04-11 17:52:33 +0300

    Home Task 6:  1. Сосчитать частоту встречаемости слов в книге War and peace.  2. Собрать все слова в группы по количеству букв: например, в одну группу попадут слова: [the, war, jar, get, met...], в другую [on, up, no, of...].  3. Вывести топ 10 самых частых слов и фраз.  4. Тоже, что и 2, но без артиклей, частиц и тп (без the, a, on, to...).
     TODO
      5. Вывести частоту встречаемости букв анг алфавита по этой книге. Вывести в процентах для каждой буквы.

---
+++ HomeTask_6/src/CollectionTraining.java
@@ -0,0 +1,149 @@
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+
+public class CollectionTraining {
+    Map<String, Integer> counterMap = new HashMap<>();
+    String fileName;
+
+    public CollectionTraining(String fileName) {
+        if (fileName == null || "".equals(fileName)) {
+            this.fileName = "HelpFiles/wp.txt";
+
+        } else {
+            this.fileName = "HelpFiles/wp.txt";
+        }
+        calcUniqueWordsCount();
+    }
+
+    public void showUniqueWordsCount() {
+        System.out.println("*******Count Unique Words!*******");
+        counterMap.entrySet().forEach(obj -> System.out.println("Word: " + obj.getKey() + " - " + obj.getValue()));
+        System.out.println("**************");
+    }
+
+    public void showTopNWords(int N) {
+        System.out.println("*******Show Top N Words!*******");
+
+        Map<String, Integer> sortedMap = counterMap.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue,
+                        (e1, e2) -> e1, LinkedHashMap::new));
+
+        sortedMap.entrySet().stream().limit(N).forEach(obj -> System.out.println("Word: " + obj.getKey() + " - " + obj.getValue()));
+
+        System.out.println("**************");
+    }
+
+
+    public void groupWordsByLetterCount() {
+        System.out.println("*******Group words by letters' count!*******");
+        Map<String, Integer> letterCount = counterMap.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Entry::getKey, stringIntegerEntry -> stringIntegerEntry.getKey().toCharArray().length,
+                (e1, e2) -> e1, LinkedHashMap::new));
+
+        letterCount.entrySet().stream()
+                .collect(groupingBy(Entry::getValue))
+                .entrySet()
+                .forEach(obj -> System.out.println("Word: " + obj.getValue()));
+
+        System.out.println("**************");
+    }
+
+
+    public void groupWordsByLetterCountWithFilter() {
+        System.out.println("*******Group words by letters' count! Without few words!*******");
+        Map<String, Integer> letterCount = counterMap.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Entry::getKey, stringIntegerEntry -> stringIntegerEntry.getKey().toCharArray().length,
+                        (e1, e2) -> e1, LinkedHashMap::new));
+
+        List<String> excludeList = Arrays.asList("1", "2", "3","4","5","6","7","8","9",
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "07", "10", "11", "12", "13", "15", "17", "18", "20", "22", "23", "24", "25", "26", "27",
+                "30", "31", "40", "50", "60", "62", "4x", "70", "80", "90", "ab", "ah", "am", "an", "as",
+                "at", "au", "ax", "be", "bl", "bs", "by", "ce", "de", "do", "dr", "ds", "du", "eh", "em",
+                "en", "et", "ex", "fo", "fr", "go", "ha", "he", "hi", "hm", "ho", "id", "if", "ii", "il",
+                "im", "in", "is", "it", "iv", "ix", "ja", "je", "ke", "la", "le", "lu", "ma", "me", "mi",
+                "mr", "my", "na", "ne", "ni", "nn", "no", "ny", "of", "oh", "on", "oo", "or", "ou", "pe",
+                "ps", "qu", "re", "rs", "sa", "se", "si", "so", "ss", "st",
+                "to", "tt", "tu", "un", "up", "us", "ve", "vi", "vy", "wa", "we", "xi", "xv", "xx", "ze", "zu");
+
+        letterCount.entrySet().stream()
+                .filter(o-> !excludeList.contains(o.getKey()))
+                .collect(groupingBy(Entry::getValue))
+                .entrySet()
+                .forEach(obj -> System.out.println("Word: " + obj.getKey() + " - " + obj.getValue()));
+
+        System.out.println("**************");
+    }
+
+
+    public void lettersCountByPercent() {
+        System.out.println("*******Show letters count by percent!*******");
+//TODO
+//       Map<String, Long> wordCount = null;
+//        try (InputStream inputStream = getClass().getResourceAsStream(fileName)) {
+//            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//            wordCount = bufferedReader.lines()
+//                   // .flatMap(line -> Arrays.stream(line.trim().split("\\s")))
+//                    .map(word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase().trim())
+//                    .filter(word -> word.length() > 0)
+//                    //.map(word->word.toCharArray())
+//                    .map(word -> new AbstractMap.SimpleEntry<>(word, 1))
+//                    .collect(groupingBy(AbstractMap.SimpleEntry::getKey, counting()));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        wordCount.forEach((k, v) -> System.out.println(String.format("%s ==>> %d", k, v)));
+
+    System.out.println("**************");
+    }
+
+
+
+    private void calcUniqueWordsCount() {
+        try (InputStream inputStream = getClass().getResourceAsStream(fileName)) {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            bufferedReader.lines()
+                    .forEach((String line) -> {
+                                String[] strings = line.toLowerCase().replaceAll("[-.?!)(,:;'\"*]", "").split("\\s");
+                                Arrays.stream(strings).forEach((String str) -> putWordCount(str));
+                            }
+                    );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void putWordCount(String word) {
+        if (!word.isEmpty()) {
+            Integer count = counterMap.get(word);
+            if (count == null) {
+                count = 0;
+            }
+            counterMap.put(word, ++count);
+        }
+    }
+
+    private int getLetterCount(String word) {
+        return word.toCharArray().length;
+    }
+}
+
+
---
+++ HomeTask_6/src/util/FileUtils.java
@@ -0,0 +1,76 @@
+package util;
+
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+
+public class FileUtils {
+
+    public static File createFile(String filePath) throws IOException {
+        if (filePath == null) {
+            throw new NullPointerException("File path is null.");
+        }
+
+        File file = new File(filePath);
+
+        if (!file.exists()) {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+
+            file.createNewFile();
+        }
+        return file;
+    }
+
+    public static void deleteFile(String filePath, boolean recursive) {
+        deleteFile(new File(filePath), recursive);
+    }
+
+    public static void deleteFile(File file, boolean recursive) {
+        if (recursive) {
+            if (file.isDirectory()) {
+                for (File f : file.listFiles()) {
+                    deleteFile(f, true);
+                }
+            }
+        }
+
+        file.delete();
+    }
+
+    public static Collection<File> getFilesFromDirectory(File directory,
+                                                         FileFilter filter,
+                                                         boolean recursive) {
+        Collection<File> container = new LinkedList<File>();
+
+        getFilesFromDirectory(directory, filter, container, recursive);
+
+        return container;
+    }
+
+    public static void getFilesFromDirectory(File directory,
+                                             FileFilter filter,
+                                             Collection<File> container,
+                                             boolean recursive) {
+        if (!directory.isDirectory()) {
+            return;
+        }
+
+        File[] files = directory.listFiles();
+
+
+        for (File file : files) {
+            if (filter.accept(file)) {
+                container.add(file);
+            }
+            if (file.isDirectory() && recursive) {
+                getFilesFromDirectory(file, filter, container, recursive);
+            }
+        }
+    }
+}
